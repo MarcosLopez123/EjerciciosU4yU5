5075804a70b68aee08ae31e414ba36e9a8be0073
LiliDiaz <lilidiaz.2816@gmail.com>
2019-11-28 18:49:00 -0600

    Ejercicios de la unidad 5

--- Ejercicios.py
+++ Ejercicios.py
@@ -14,6 +14,12 @@ for linea in archivo:
     texto += linea
 print(texto)
 
+#Ejercicio 1
+
+patron = r"[a-zA-Z][\w\d]*"
+result1 = re.findall(patron,texto)
+#print ("\nVariables: ", result)
+
 #Ejercicio 2
 patron = r"(\b[0-9]{1,}\.\b[0-9]{1,}|\b[0-9]{1,}\b)"
 result = re.findall(patron,texto)
@@ -40,3 +46,35 @@ print ("\n Operadores relacionales encontrados: ", result)
 patron = r"\b(public|static|void|for|while|System|case|import|util|scanner|int|out|System|String|Double)\b"
 result = re.findall(patron,texto)
 print ("\nPalabras reservadas de JAVA: ", result)
+
+#Ejercicio 6
+a="[\+|\-|\/|\*|\%]"#para concatenar la expresion regular en los ejercicios 
+b="[\d+\.\d+|\w]+"
+patron = r"(\w+( |)=( |)"+b+")"
+result = re.findall(patron,texto)
+print ("\nSentencias de Asignacion: ", result)
+
+#Ejercicio 7
+patron= r"(\w+( |)=( |)"+b+"( |)"+a+"( |)"+b+")"
+result = re.findall(patron,texto)
+print ("\nOperaciones aritméticas básicas: ", result)
+
+
+#Ejercicio 8
+patron= r"([ |](\={2,3}|\>|\<|\!|\>=|\<=|\!=)[ |]"+b+")"
+result = re.findall(patron,texto)
+print ("\nExpresiones booleanas básicas: ", result)
+
+#Ejercicio 9
+patron=r'(\w+( |)=( |)[\d+\.\d+|\w]+( |)[\+|\-|\/|\*|\%]( |)[\(\d+\.\d+|\w]+( |)[\+|\-|\/|\*|\%]( |)[\d+\.\d+|\w]+\));'
+result = re.findall(patron,texto)
+print ("\nFormulas más complejas : ", result)
+
+#Ejercicio 10
+patron= r"\b(if|for|while|switch|forEach)\b"
+result = re.findall(patron,texto)
+print ("\nEl encabezado de estructura de control: ", result)
+
+for i in result1:
+    if (i!="import" and i!="int" and i!="double" and i!="float" and i!="static" and i!="public" and i!="void" and i!="for" and i!="System" and i!="while" and i!="case" and i!="Scanner" and i!="util" and i!="String" and i!="println" and i!="java" and i!="class" and i!="main" and i!="args" and i!="out" and i!="if" and i!="else"):
+        print("Variables válidas: "+i)
